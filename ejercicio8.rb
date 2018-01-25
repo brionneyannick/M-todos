7a07ee64fc666608012624de1fd302064bc27caf
Yannick <brionneyannick@gmail.com>
2018-01-25 10:12:05 -0300

    Ejercicio ocho mejorado

--- ejercicio8.rb
+++ ejercicio8.rb
@@ -1,24 +1,25 @@
 alumnos = ['Hugo', 'Felipe', 'Luis', 'Claudio', 'Adrian', 'Patricia', 'Yannick', 'Fernanda', 'Franco', 'Felipe', 'Heraldo', 'Arturo', 'Milenko', 'Daniel', 'Daniel', 'Ignacio', 'Kevin', 'Norman', 'Roberto', 'Patricio', 'Matias']
 
-alumnos.each do |i|
-  puts i if i.length > 5
-end
+# alumnos.each do |i|
+#   puts i if i.length > 5
+# end
+#
+#
+#
+# copia_alumnos = []
+# alumnos.each do |i|
+#   copia_alumnos.push(i.downcase)
+# end
+# print copia_alumnos
+#
 
-
-
-copia_alumnos = []
-alumnos.each do |i|
-  copia_alumnos.push(i.downcase)
-end
-print copia_alumnos
-
-
-array_letras = []
-def letras(*alumnos)
+def letras(var_del_metodo)
   array_letras = []
-  alumnos.each do |i|
+  var_del_metodo.each do |i|
     array_letras.push(i.length)
   end
-  print array_letras
+  array_letras
 end
-letras(*alumnos)
+letras_arr = []
+letras_arr = letras(alumnos)
+print letras_arr
